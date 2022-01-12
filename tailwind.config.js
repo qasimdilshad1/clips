e844d3468176b6174d5e28d15d0351d4e4eb7971
qasim.dilshad@systemsltd.com <qasim.dilshad@systemsltd.com>
2022-01-12 15:43:55 +0500

    tailwind applied

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge:{
+    content:[
+      './src/**/*.{html,ts}'
+    ]
+    
+  },
+  darkmode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
