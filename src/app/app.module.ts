717e5606a9d8b6ee55a7290f64245eb5da21305b
qasim.dilshad@systemsltd.com <qasim.dilshad@systemsltd.com>
2022-01-13 15:29:16 +0500

    user-auth modal added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
 import { NavComponent } from './nav/nav.component';
-import { AuthModalComponent } from './user/auth-modal/auth-modal.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent, NavComponent, AuthModalComponent,
+    AppComponent, NavComponent, 
   ],
   imports: [
     BrowserModule,
