77c7a38f07f75c74355866ebe0b9189aecbc1712
qasim.dilshad@systemsltd.com <qasim.dilshad@systemsltd.com>
2022-01-17 16:26:47 +0500

    tabs component created

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 //import { ModalService } from '../services/modal.service';
 
 
 
 @NgModule({
   declarations:[
-    ModalComponent
+    ModalComponent,
+    TabsContainerComponent
   ],
   imports:[
     CommonModule
