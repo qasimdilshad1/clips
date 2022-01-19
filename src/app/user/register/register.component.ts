d0ec05064f4ce31093384034ca51c7265932e474
qasim.dilshad@systemsltd.com <qasim.dilshad@systemsltd.com>
2022-01-19 18:22:48 +0500

    name field validation completed

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent  {
+  name= new FormControl('',[
+       Validators.required,
+       Validators.minLength(3)
+       
+   ] )
+     email= new FormControl('')
+     age= new FormControl('') 
+     password= new FormControl('') 
+     confirm_password= new FormControl('')
+     phoneNumber= new FormControl('')  
 
-  constructor() { }
+registerForm = new FormGroup({
+   name: this.name ,
+   email: this.email,
+   age: this.age,
+   password: this.password,
+   confirm_password: this.confirm_password,
+   phoneNumber: this.phoneNumber
+})
 
-  ngOnInit(): void {
-  }
-
+ 
 }
