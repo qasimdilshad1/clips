b379f96fd6a0140e78f112b0dc71801569dc9181
qasim.dilshad@systemsltd.com <qasim.dilshad@systemsltd.com>
2022-01-20 15:13:19 +0500

    email validation

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
-
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.css'],
 })
 export class InputComponent implements OnInit {
-  @Input() control: FormControl = new FormControl()
+  @Input() control: FormControl = new FormControl();
+  @Input() type = 'text';
+  @Input() placeholder = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
