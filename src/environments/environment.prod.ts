0a8fef3257aa1e90a4190de4f439514af4e22d54
qasim.dilshad@systemsltd.com <qasim.dilshad@systemsltd.com>
2022-02-01 14:25:22 +0500

    firebase installed and imported

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBBaVCAOqFA4m_KUxkPLm0H6VqEgg1xv98',
+    authDomain: 'clips-55984.firebaseapp.com',
+    projectId: 'clips-55984',
+    storageBucket: 'clips-55984.appspot.com',
+    messagingSenderId: '485117506474',
+    appId: '1:485117506474:web:9f3052eee3309f32dd31d3',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBBaVCAOqFA4m_KUxkPLm0H6VqEgg1xv98',
+    authDomain: 'clips-55984.firebaseapp.com',
+    projectId: 'clips-55984',
+    storageBucket: 'clips-55984.appspot.com',
+    messagingSenderId: '485117506474',
+    appId: '1:485117506474:web:9f3052eee3309f32dd31d3',
+  },
 };
 
 /*
