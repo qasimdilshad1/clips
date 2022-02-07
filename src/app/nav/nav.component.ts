dd05f1e3584ef858b49c96bd53d956b16e5afcf5
qasim.dilshad@systemsltd.com <qasim.dilshad@systemsltd.com>
2022-02-07 17:44:35 +0500

    signout complete

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-  constructor(public modal: ModalService, public auth: AuthService) {
+  constructor(public modal: ModalService, public auth: AuthService, public afAuth:AngularFireAuth) {
   
   }
 
@@ -16,5 +17,9 @@ export class NavComponent implements OnInit {
   openModal($event: Event) {
     $event.preventDefault();
     this.modal.toggleModal('auth');
+  }
+  async logout($event:Event){
+    $event.preventDefault
+    await this.afAuth.signOut()
   }
 }
