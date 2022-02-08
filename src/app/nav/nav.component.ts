7b06a9a677a19b1cac0496fa3fa095a28002b2ca
qasim.dilshad@systemsltd.com <qasim.dilshad@systemsltd.com>
2022-02-08 19:27:04 +0500

    routes activated

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-  constructor(public modal: ModalService, public auth: AuthService, public afAuth:AngularFireAuth) {
-  
-  }
+  constructor(
+    public modal: ModalService,
+    public auth: AuthService,
+    
+  ) {}
 
   ngOnInit(): void {}
 
@@ -18,8 +20,5 @@ export class NavComponent implements OnInit {
     $event.preventDefault();
     this.modal.toggleModal('auth');
   }
-  async logout($event:Event){
-    $event.preventDefault
-    await this.afAuth.signOut()
-  }
+  
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,7 +7,8 @@ import {
 import { Observable } from 'rxjs';
 import Iuser from '../models/user.model';
 import {map,delay} from  'rxjs/operators' 
-
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -17,7 +18,7 @@ export class AuthService {
   public isAuthenticated$: Observable<boolean>
   public isAuthenticatedWithDelay$:Observable<boolean>
 
-  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
+  constructor(private auth: AngularFireAuth, private db: AngularFirestore,private router: Router, private route:ActivatedRoute) {
     this.usersCollection = db.collection('users');
     auth.user.subscribe(console.log)
     this.isAuthenticated$=auth.user.pipe(
@@ -50,5 +51,14 @@ export class AuthService {
       displayName:userData.name
     });
    
+  }   
+      public async logout($event?: Event){
+        if($event){
+          $event.preventDefault;
+        }
+     
+      await this.auth.signOut();
+      await this.router.navigateByUrl('/')
+    }
+
   }
-}
