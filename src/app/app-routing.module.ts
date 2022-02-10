b112b452435e2289b2cbc8bc822718ca232d972b
qasim.dilshad@systemsltd.com <qasim.dilshad@systemsltd.com>
2022-02-10 14:59:21 +0500

    route parameters subscribed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ClipComponent } from './clip/clip.component';
 
 const routes: Routes = [{
   path:'',
@@ -10,6 +11,11 @@ const routes: Routes = [{
 {
   path:'about',
   component: AboutComponent
+},
+{
+  path:'clip/:id',
+  component:ClipComponent
+
 }];
 
 @NgModule({
